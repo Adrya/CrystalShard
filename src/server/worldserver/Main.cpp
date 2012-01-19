44c74c7b334a6a471c3e64ab2de7c9af0aa586fc
puscher <minipc@hotmail.de>
2012-01-19 23:03:17 +0100

    Core/Misc: Renamend TrinityCore to CrystalShard

--- src/server/worldserver/Main.cpp
+++ src/server/worldserver/Main.cpp
@@ -38,8 +38,8 @@
 #ifdef _WIN32
 #include "ServiceWin32.h"
 char serviceName[] = "worldserver";
-char serviceLongName[] = "TrinityCore world service";
-char serviceDescription[] = "TrinityCore World of Warcraft emulator world service";
+char serviceLongName[] = "CrystalShard world";
+char serviceDescription[] = "CrystalShard WoW emu world service";
 /*
  * -1 - not in service mode
  *  0 - stopped
